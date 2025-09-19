c0a944f83d31f624c24242ae644c6913cd61b4c3
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2025-09-19 21:30:14 +0530

    Time: 2 ms (48.55%), Space: 12.7 MB (60.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-   vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int> nexti(m,-1);
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        unordered_map<int,int> m;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(s.size()>0 && s.top()<=nums2[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                m[nums2[i]]=-1;
+            }else{
+                m[nums2[i]]=s.top();
+            }
+            s.push(nums2[i]);
+        }
+
         vector<int> ans;
-        unordered_map<int,int> mp;
-        stack<int> st;
-        st.push(-1);
-        for(int i=m - 1;i>=0;i--){
-            while(!st.empty() && st.top() <= nums2[i]){
-                st.pop();
-            }
-            if(!st.empty()){
-                nexti[i] = st.top();
-            }
-            st.push(nums2[i]);
-        }
-        for(int i=0;i<m;i++){
-            mp[nums2[i]] = i;
-        }
-        for(int i=0;i<n;i++){
-             int indextofind = nums1[i];
-             int indexis = mp[indextofind];
-             ans.push_back(nexti[indexis]);
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(m[nums1[i]]);
         }
         return ans;
     }
