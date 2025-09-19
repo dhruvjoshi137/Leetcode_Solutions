d4cda3bda95e2db559699398a9ec0fbc2735aa98
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2025-09-19 16:03:06 +0530

    Time: 1 ms (55.83%), Space: 12.9 MB (37.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-  vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    unordered_map<int, int> mp;
-    stack<int> st;
-
-    for(int i=nums2.size()-1; i>=0; i--){
-      while(!st.empty() && st.top() < nums2[i]){
-        st.pop();
-      }
-      if(st.empty() || st.top() < nums2[i]) mp[nums2[i]] = -1;
-      else mp[nums2[i]] = st.top();
-      st.push(nums2[i]);
+   vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int> nexti(m,-1);
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        stack<int> st;
+        st.push(-1);
+        for(int i=m - 1;i>=0;i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                nexti[i] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<m;i++){
+            mp[nums2[i]] = i;
+        }
+        for(int i=0;i<n;i++){
+             int indextofind = nums1[i];
+             int indexis = mp[indextofind];
+             ans.push_back(nexti[indexis]);
+        }
+        return ans;
     }
-
-    vector<int> solution;
-    for(int i=0; i<nums1.size(); i++){
-      solution.push_back(mp[nums1[i]]);
-    }
-
-
-    return solution;
-  }
 };
