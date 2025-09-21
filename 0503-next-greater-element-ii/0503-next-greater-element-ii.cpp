71a2c698954aa814d04a3a0a3012f5276331ca22
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2025-09-21 15:57:49 +0530

    Time: 126 ms (5.04%), Space: 28 MB (95.04%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return {};
-        vector<int> res(n, -1);
-        vector<int> st; // acts as stack storing values in decreasing order
-
-        // iterate from right to left across a logical doubled array
-        for (int i = 2 * n - 1; i >= 0; --i) {
-            int cur = nums[i % n];
-            while (!st.empty() && st.back() <= cur) st.pop_back();
-            if (i < n) res[i] = st.empty() ? -1 : st.back();
-            st.push_back(cur);
+        vector<int> nge(nums.size(),-1);
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<i+nums.size();j++){
+                int ind = j%nums.size();
+                if(nums[ind]>nums[i]){
+                    nge[i]=nums[ind];
+                    break;
+                }
+            }
         }
-        return res;
+            return nge;
     }
 };
