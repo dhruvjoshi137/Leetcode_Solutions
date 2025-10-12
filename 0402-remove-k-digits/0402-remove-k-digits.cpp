5bbde65144f98de43815372122f95f358fc58b9e
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2025-10-12 22:57:07 +0530

    Time: 3 ms (63.54%), Space: 10.9 MB (94.92%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-
-        // stack<char> st;
         string st = "";
 
         for(char c : num){
@@ -12,27 +10,22 @@ public:
             }
             st.push_back(c);
         }
+            while(k>0 && !st.empty()){
+                st.pop_back();
+                k--;
+            }
 
-        //this next step is a safety step , agar meri string already increasing order mein hui
-        //toh mera k kabhi bhi decrement nhi krega aur loop ke bad k is the same 
-        //in the case of 12345 , koi remove nahi hua saaere stack ke ander chale gaye toh 
-        //i will pop out k times so that number remains the smallest 
-        while(k>0 && !st.empty()){
-            st.pop_back();
-            k--;
-        }
+            int idx = 0;
+            while(idx<st.size() && st[idx]=='0'){
+                idx++;
+            }
 
-        int idx=0;
-
-        while(idx<st.size() && st[idx]=='0'){
-            idx++;
-        }
-        string res = st.substr(idx);
-
-        if(res.empty()){
-            return "0";
-        }else{
-            return res;
-        }
+            string res = st.substr(idx);
+            if(res.empty()){
+                return "0";
+            }else{
+                return res;
+            }
+        
     }
 };
