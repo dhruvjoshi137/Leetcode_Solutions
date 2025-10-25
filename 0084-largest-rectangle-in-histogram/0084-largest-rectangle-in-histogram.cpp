35cdd11ae2ba2cee413d2a282a8d627e4d51cf95
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2025-10-25 18:16:35 +0530

    Time: 72 ms (5.17%), Space: 96.5 MB (5.37%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,38 +1,64 @@
 class Solution {
 public:
-  int largestRectangleArea(vector < int > & heights) {
-      int n = heights.size();
-      stack < int > st;
-      int leftsmall[n], rightsmall[n];
-      for (int i = 0; i < n; i++) {
-        while (!st.empty() && heights[st.top()] >= heights[i]) {
-          st.pop();
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> left;
+        vector<int> right;
+        stack<pair<int,int>> s;
+
+        for(int i=0;i<n;i++){
+        int pseudoIndex = -1;
+            if(s.size()==0){
+                left.push_back(pseudoIndex);
+            }else if(s.size()>0 && s.top().first<heights[i]){
+                left.push_back(s.top().second);
+            }else if(s.size()>0 && s.top().first>=heights[i]){
+                while(s.size()>0 && s.top().first>=heights[i]){
+                    s.pop();
+                }
+                if(s.size()==0){
+                    left.push_back(pseudoIndex);
+                }else{
+                    left.push_back(s.top().second);
+                }
+            }
+            s.push({heights[i],i});
         }
-        if (st.empty())
-          leftsmall[i] = 0;
-        else
-          leftsmall[i] = st.top() + 1;
-        st.push(i);
-      }
-      // clear the stack to be re-used
-      while (!st.empty())
-        st.pop();
 
-      for (int i = n - 1; i >= 0; i--) {
-        while (!st.empty() && heights[st.top()] >= heights[i])
-          st.pop();
+        //clear the stack first
+        while(!s.empty()){
+            s.pop();
+        }
 
-        if (st.empty())
-          rightsmall[i] = n - 1;
-        else
-          rightsmall[i] = st.top() - 1;
+        for(int i=n-1;i>=0;i--){
+        int pseudoIndex = n;
+            if(s.size()==0){
+                right.push_back(pseudoIndex);
+            }else if(s.size()>0 && s.top().first<heights[i]){
+                right.push_back(s.top().second);
+            }else if(s.size()>0 && s.top().first>=heights[i]){
+                while(s.size()>0 && s.top().first>=heights[i]){
+                    s.pop();
+                }
+                if(s.size()==0){
+                    right.push_back(pseudoIndex);
+                }else{
+                    right.push_back(s.top().second);
+                }
+            } 
+            s.push({heights[i],i});
+        }
+            reverse(right.begin(),right.end());
 
-        st.push(i);
-      }
-      int maxA = 0;
-      for (int i = 0; i < n; i++) {
-        maxA = max(maxA, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
-      }
-      return maxA;
+  
+
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            int width = right[i]-left[i]-1;
+            int area = heights[i]*width;
+            maxArea = max(maxArea,area);
+        }
+
+        return maxArea;
     }
 };
