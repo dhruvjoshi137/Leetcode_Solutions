07dd5a7068f646e9fa622234f9977e0e1f9eb1cc
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2025-12-23 14:38:36 +0530

    Time: 8 ms (39.86%), Space: 11.6 MB (36.63%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -5,36 +5,41 @@ public:
         for(char ch : p){
             mp[ch]++;
         }
-        int n = s.size();
-        int i=0,j=0;
-        vector<int> ans;
+
+        int i=0 , j = 0;
+        vector<int> ans; //final resultant vector which will contain the starting indeices of all the anagrams 
+        int n = s.length(); //string size
+        int k = p.length(); //window size
         int count = mp.size();
-        int k = p.size();
 
         while(j<n){
-            if(mp.find(s[j]) != mp.end()){
+            if(mp.find(s[j]) !=mp.end()){
                 mp[s[j]]--;
                 if(mp[s[j]]==0){
-                count--;
+                    count--;
+                }
             }
-            }
-           
 
             if(j-i+1 < k){
                 j++;
-            }else if(j-i+1 == k){
+            }
+
+            else if(j-i+1 == k){
                 if(count==0){
                     ans.push_back(i);
                 }
+
                 if(mp.find(s[i]) != mp.end()){
                     if(mp[s[i]]==0){
-                    count++;
+                        count++;
                     }
-                mp[s[i]]++;
+                    mp[s[i]]++;
                 }
-            i++;
-            j++;
+
+                i++;
+                j++;
             }
+
         }
 
         return ans;
