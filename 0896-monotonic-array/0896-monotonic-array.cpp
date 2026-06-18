e277cd17b73c42484d673db15fcb86bb6c085e67
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2026-06-18 19:04:52 +0530

    Time: 0 ms (100%), Space: 100.4 MB (32.79%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -4,6 +4,8 @@ public:
         bool increasing = true;
         bool decreasing = true;
 
+        if(nums.size() <= 1) return true;
+
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i] < nums[i+1]) decreasing = false;
             if(nums[i] > nums[i+1]) increasing = false;
