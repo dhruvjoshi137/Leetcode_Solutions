d730aa9f52a6cac67f93654b0efc101ddf918498
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2026-06-25 12:52:41 +0530

    Time: 42 ms (63.62%), Space: 39.5 MB (96.08%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -4,12 +4,18 @@ public:
         int n = nums.size();
         int count = 0;
         for(int i=0;i<n;i++){
+            //iterate through every possible starting point of an array
             int cnt = 0;
             for(int j=i;j<n;j++){
+                //iterate thorugh every pssible ending point of the array
                 if(nums[j] == target){
+                //check if the current element matches the target
                     cnt++;
+                    //increase the count as the target matches the element
                 }
+                
                 if(cnt > (j-i+1)/2) count++;
+                //check if the presence of the target is majority by checking if the count is strictly more than half of the length and increase the count if it is so
             }
         }
         return count;
