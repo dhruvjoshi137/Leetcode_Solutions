071d52347e849f7dd3e799b84c1db3f6b8d2b709
Dhruv Sharma <150547437+dhruvjoshi137@users.noreply.github.com>
2026-06-25 16:23:24 +0530

    Time: 0 ms (100%), Space: 9.5 MB (29.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> v(m, vector<int>(n, 1));
-
-        for(int i=1; i<m; i++) {
-            for(int j=1; j<n; j++) {
-                v[i][j] = v[i-1][j] + v[i][j-1];
+        //this question is based on one at a time / either this or that 
+        // so this basically means this can be solve using dp
+        vector<vector<int>> dp(m , vector<int> (n , 1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return v[m-1][n-1];
+        return dp[m-1][n-1];
     }
 };
